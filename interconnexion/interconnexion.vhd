12185b25407582e1b153b2b02d310d08eeeb1e5e
Corentin-k <c.kervagoret@outlook.fr>
2025-04-29 09:14:08 +0200

    correcction interconnexion

--- interconnexion/interconnexion.vhd
+++ interconnexion/interconnexion.vhd
@@ -2,7 +2,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
+use ieee.std_logic_signed.all;
 entity interconnexion is
     port(
         -- SEL_ROUTE permet de définir le transfert de données qui sera effectué lors du prochain cycle horloge (prochain front montant de l’horloge).
@@ -15,11 +15,11 @@ entity interconnexion is
         -- La mémoire MEM_SEL_FCT permet de mémoriser la fonction arithmétique ou logique à réaliser.
         -- Elle est systématiquement chargée à chaque front montant d’horloge.
         MEM_CACHE_1_in: in std_logic_vector(7 downto 0); -- Mémoire cache 1
-        MEM_CACHE_1_out_enable : in std_logic; -- Signal d'activation pour MEM_CACHE_1_ou
+        MEM_CACHE_1_out_enable : out std_logic; -- Signal d'activation pour MEM_CACHE_1_ou
         MEM_CACHE_1_out : out std_logic_vector(7 downto 0); -- Sortie vers MEM_CACHE_1_out
 
         MEM_CACHE_2_in : in std_logic_vector(7 downto 0); -- Mémoire cache 2
-        MEM_CACHE_2_out_enable : in std_logic; -- Signal d'activation pour MEM_CACHE_2_out_enable
+        MEM_CACHE_2_out_enable : out std_logic; -- Signal d'activation pour MEM_CACHE_2_out_enable
         MEM_CACHE_2_out : out std_logic_vector(7 downto 0); -- Sortie vers MEM_CACHE_2_out
         
         -- Les mémoires Buffer_A, Buffer_B permettent de stocker les données directement liées au cœur de l’UAL, c'est-à-dire à la sous-fonction arithmétique et logique. 
@@ -31,7 +31,7 @@ entity interconnexion is
         Buffer_B_enable : out std_logic; -- Signal d'activation pour Buffer B
 
         SEL_OUT : in std_logic_vector(1 downto 0); -- Sélecteur de sortie
-        RES_OUT : out std_logic_vector(7 downto 0); -- Sortie 
+        RES_OUT : out std_logic_vector(7 downto 0) -- Sortie 
 
 
     );
@@ -45,16 +45,13 @@ begin
 
     selRouteProcess : process(SEL_ROUTE, A_IN, B_IN, S, MEM_CACHE_1_in, MEM_CACHE_2_in) 
     begin
-
-
         case SEL_ROUTE is
-
             when "0000" =>  -- Stockage de l'entrée A_IN dans Buffer_A
                 Buffer_A <=A_IN;
                 Buffer_A_enable <= '1'; 
 
                 Buffer_B <= (others => '0');
-                Buffer_B_enable <= (others => '0'); 
+                Buffer_B_enable <= '0';
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -66,8 +63,8 @@ begin
                 Buffer_A <= MEM_CACHE_1_in(3 downto 0);
                 Buffer_A_enable <= '1'; 
 
-                Buffer_B <='0';
-                Buffer_B_enable <= (others => '0'); 
+                Buffer_B <=(others => '0');
+                Buffer_B_enable <= '0';
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -79,8 +76,8 @@ begin
                 Buffer_A <= MEM_CACHE_1_in(7 downto 4);
                 Buffer_A_enable <= '1'; 
                 
-                Buffer_B <='0';
-                Buffer_B_enable <= (others => '0'); 
+                Buffer_B <=(others => '0');
+                Buffer_B_enable <= '0';
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -93,8 +90,8 @@ begin
                 Buffer_A <= MEM_CACHE_2_in(3 downto 0);
                 Buffer_A_enable <= '1';
 
-                Buffer_B <='0';
-                Buffer_B_enable <= (others => '0'); 
+                Buffer_B <=(others => '0');
+                Buffer_B_enable <= '0';
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -107,8 +104,8 @@ begin
                 Buffer_A <= MEM_CACHE_2_in(7 downto 4);
                 Buffer_A_enable <= '1'; 
 
-                Buffer_B <='0';
-                Buffer_B_enable <= (others => '0'); 
+                Buffer_B <=(others => '0');
+                Buffer_B_enable <= '0';
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -120,8 +117,8 @@ begin
                 Buffer_A <= S(3 downto 0);
                 Buffer_A_enable <= '1'; 
 
-                Buffer_B <='0';
-                Buffer_B_enable <= (others => '0'); 
+                Buffer_B <=(others => '0');
+                Buffer_B_enable <= '0';
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -133,8 +130,8 @@ begin
                 Buffer_A <= S(7 downto 4);
                 Buffer_A_enable <= '1'; 
 
-                Buffer_B <='0';
-                Buffer_B_enable <= (others => '0'); 
+                Buffer_B <=(others => '0');
+                Buffer_B_enable <= '0';
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -147,8 +144,8 @@ begin
                 Buffer_B <=b_IN;
                 Buffer_B_enable <= '1'; 
                 
-                Buffer_A <='0';
-                Buffer_A_enable <= (others => '0'); 
+                Buffer_A <=(others => '0');
+                Buffer_A_enable <= '0';
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -161,8 +158,8 @@ begin
                 Buffer_B <= MEM_CACHE_1_in(3 downto 0);
                 Buffer_B_enable <= '1'; 
 
-                Buffer_A <='0';
-                Buffer_A_enable <= (others => '0'); 
+                Buffer_A <=(others => '0');
+                Buffer_A_enable <= '0';
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -174,8 +171,8 @@ begin
                 Buffer_B <= MEM_CACHE_1_in(7 downto 4);
                 Buffer_B_enable <= '1'; 
 
-                Buffer_A <='0';
-                Buffer_A_enable <= (others => '0'); 
+                Buffer_A <=(others => '0');
+                Buffer_A_enable <= '0'; 
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -187,8 +184,8 @@ begin
                 Buffer_B <= MEM_CACHE_2_in(3 downto 0);
                 Buffer_B_enable <= '1'; 
 
-                Buffer_A <='0';
-                Buffer_A_enable <= (others => '0'); 
+                Buffer_A <=(others => '0');
+                Buffer_A_enable <= '0'; 
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -200,8 +197,8 @@ begin
                 Buffer_B <= MEM_CACHE_2_in(7 downto 4);
                 Buffer_B_enable <= '1';
 
-                Buffer_A <='0';
-                Buffer_A_enable <= (others => '0'); 
+                Buffer_A <=(others => '0');
+                Buffer_A_enable <= '0';
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -213,8 +210,8 @@ begin
                 Buffer_B <= S(3 downto 0);
                 Buffer_B_enable <= '1'; 
 
-                Buffer_A <='0';
-                Buffer_A_enable <= (others => '0'); 
+                Buffer_A <=(others => '0');
+                Buffer_A_enable <= '0'; 
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -226,8 +223,8 @@ begin
                 Buffer_B <= S(7 downto 4);
                 Buffer_B_enable <= '1'; 
 
-                Buffer_A <='0';
-                Buffer_A_enable <= (others => '0'); 
+                Buffer_A <=(others => '0');
+                Buffer_A_enable <= '0'; 
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -236,39 +233,39 @@ begin
                 MEM_CACHE_2_out_enable <= '0';
 
             when "1110" => -- Stockage de S dans MEM_CACHE_1_in
-                MEM_CACHE_1_out(3 downto 0)=>S ;
+                MEM_CACHE_1_out(7 downto 0)<=S ;
                 MEM_CACHE_1_out_enable <= '1';
 
                 MEM_CACHE_2_out <= (others => '0');
                 MEM_CACHE_2_out_enable <= '0';
 
                 Buffer_A <= (others => '0');
-                Buffer_A_enable <= (others => '0'); 
+                Buffer_A_enable <='0';
 
                 Buffer_B <= (others => '0');
-                Buffer_B_enable <= (others => '0');
+                Buffer_B_enable <= '0';
 
             when "1111" => -- Stockage de S dans MEM_CACHE_2
                 
-                MEM_CACHE_2_out(3 downto 0)=>S ;
+                MEM_CACHE_2_out(7 downto 0)<=S ;
                 MEM_CACHE_2_out_enable <= '1';
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
 
                 BUFFER_A <= (others => '0');
-                BUFFER_A_enable <= (others => '0');
+                BUFFER_A_enable <= '0';
 
                 BUFFER_B <= (others => '0');
-                BUFFER_B_enable <= (others => '0');
+                BUFFER_B_enable <= '0';
 
 
             when others => -- Valeurs par défaut
                 Buffer_A <= (others => '0');
-                Buffer_A_enable <= (others => '0'); 
+                Buffer_A_enable <= '0';
 
                 Buffer_B <= (others => '0');
-                Buffer_B_enable <= (others => '0');
+                Buffer_B_enable <= '0';
 
                 MEM_CACHE_1_out <= (others => '0');
                 MEM_CACHE_1_out_enable <= '0';
@@ -278,12 +275,12 @@ begin
             
 
             end case;
-    end process selRouteProcess;
+    end process;
 
-    selOutProcess : process(SEL_OUT, S, MEM_CACHE_1_in, MEM_CACHE_2_in)
+    selOutProcess: process(SEL_OUT, S, MEM_CACHE_1_in, MEM_CACHE_2_in)
     begin
         case SEL_OUT is
-            when "00" =>  -- Aucune sortie 
+            when "00" => 
                 RES_OUT <= (others => '0');
             when "01" => -- Sortie vers MEM_CACHE_1_in
                 RES_OUT <= MEM_CACHE_1_in;
@@ -291,6 +288,7 @@ begin
                 RES_OUT <= MEM_CACHE_2_in;
             when "11" => -- Sortie vers S
                 RES_OUT <= S;
+            when others => RES_OUT <= (others => '0'); 
         end case;
     end process selOutProcess;
 end interconnexion_arch;
