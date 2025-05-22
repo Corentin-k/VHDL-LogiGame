b34e73e95636deab1367d94d1bba7bcc3e58c0a1
Corentin-k <c.kervagoret@outlook.fr>
2025-05-22 12:01:51 +0200

    ajout d'un script d'automatisation des commande ghdl

---
+++ run_vhdl.sh
@@ -0,0 +1,29 @@
+﻿#!/bin/bash
+# chmod +x run_vhdl.sh
+# ./run_vhdl.sh nom_dossier [--g]
+
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 nom_dossier [--g]"
+  exit 1
+fi
+
+DIR="$1"
+TB_ENTITY="${DIR}_tb"
+VHD="${DIR}.vhd"
+TB_VHD="${DIR}_testbench.vhd"
+VCD="${DIR}_gtkwave.vcd"
+GTK=0
+
+if [ "$2" == "--g" ]; then
+  GTK=1
+fi
+
+cd "$DIR" || { echo "Dossier $DIR introuvable"; exit 1; }
+
+ghdl -a "$VHD" "$TB_VHD" || exit 1
+ghdl -e "$TB_ENTITY" || exit 1
+ghdl -r "$TB_ENTITY" --vcd="$VCD" || exit 1
+
+if [ $GTK -eq 1 ]; then
+  gtkwave "$VCD"
+fi
