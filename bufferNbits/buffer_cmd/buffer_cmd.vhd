c9ddef45ba0c5d3a0914537ef864be13e2c64ba1
Corentin-k <c.kervagoret@outlook.fr>
2025-05-25 13:14:38 +0200

    correction buffers

---
+++ bufferNbits/buffer_cmd/buffer_cmd.vhd
@@ -0,0 +1,31 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+-- pour SEL_FCT et SEL_ROUTE
+entity buffer_cmd is
+
+port (
+	e1 : in std_logic_vector (3 downto 0);
+    reset : in std_logic;
+    clock : in std_logic;
+    s1 : out std_logic_vector (3 downto 0);
+
+);
+end  buffer_cmd;
+
+architecture buffer_cmd_arch of buffer_cmd is
+	    
+begin
+	 
+    process(clock, reset)
+    begin
+        -- Reset asynchrone sur niveau haut
+        if reset = '1' then
+            s1 <= (others => '0');
+        elsif rising_edge(clock) then
+                s1 <= e1;
+        end if;
+        
+    end process;
+   
+end buffer_cmd_arch;
+
