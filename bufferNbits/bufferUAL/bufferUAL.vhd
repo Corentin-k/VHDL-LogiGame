c2ff5ec8ad6c368d4c4bfa9cf862da7b23b7a606
Corentin-k <c.kervagoret@outlook.fr>
2025-05-25 12:51:10 +0200

    simplify bufferUAL en ajoutant une varaible pour la taille  du buffer

--- bufferNbits/bufferUAL/bufferUAL.vhd
+++ bufferNbits/bufferUAL/bufferUAL.vhd
@@ -2,25 +2,18 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity buffer_ual is
-
+generic (
+    N : integer := 4  
+);
 port (
-	e1 : in std_logic_vector (7 downto 0);
+	e1 : in std_logic_vector (N-1 downto 0);
     reset : in std_logic;
    
     clock : in std_logic;
 
+    enable : in std_logic;
+    s1 : out std_logic_vector (N-1 downto 0)
     
-    MEM_CACHE_1_out_enable : in std_logic;
-    MEM_CACHE_1_out        : out std_logic_vector(7 downto 0);
-    
-    MEM_CACHE_2_out_enable : in std_logic;
-    MEM_CACHE_2_out        : out std_logic_vector(7 downto 0);
-
-    Buffer_A               : in std_logic_vector(3 downto 0);
-    Buffer_A_enable        : out std_logic;
-
-    Buffer_B               : in std_logic_vector(3 downto 0);
-    Buffer_B_enable        : out std_logic;
 );
 end  buffer_ual; 
 
@@ -28,48 +21,16 @@ architecture buffer_ual_arch of buffer_ual is
 
 begin
 
-    BufferMemCache1Proc: process(clock, reset)
+    process(clock, reset)
     begin
         -- Reset asynchrone sur niveau haut
         if reset = '1' then
-            MEM_CACHE_1_out <= (others => '0');
-        elsif rising_edge(clock) and MEM_CACHE_1_out_enable ='1' then
-                MEM_CACHE_1_out <= e1;
+            s1 <= (others => '0');
+        elsif rising_edge(clock) and enable ='1' then
+                s1 <= e1;
         end if;
-        
-    end process BufferMemCache1Proc;
 
-    BufferMemCache2Proc: process(clock, reset)
-    begin
-        -- Reset asynchrone sur niveau haut
-        if reset = '1' then
-            MEM_CACHE_2_out <= (others => '0');
-        elsif rising_edge(clock) and MEM_CACHE_2_out_enable ='1' then
-                MEM_CACHE_2_out <= e1;
-        end if;
-        
-    end process BufferMemCache2Proc;
+    end process;
 
-    BufferAProc: process(clock, reset)
-    begin
-        -- Reset asynchrone sur niveau haut
-        if reset = '1' then
-            Buffer_A <= (others => '0');
-        elsif rising_edge(clock) and Buffer_A_enable ='1' then
-                Buffer_A <= e1;
-        end if;
-        
-    end process BufferAProc;
-
-    BufferBProc: process(clock, reset)
-    begin
-        -- Reset asynchrone sur niveau haut
-        if reset = '1' then
-            Buffer_B <= (others => '0');
-        elsif rising_edge(clock) and Buffer_B_enable ='1' then
-                Buffer_B <= e1;
-        end if;
-        
-    end process BufferBProc;
 end buffer_ual_arch;
 
